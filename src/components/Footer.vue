05916a5c3e875ca482d6b17047c636e3639fbd96
HakimYu <2263197083@qq.com>
2022-03-11 23:22:39 +0800

    添加备案号并build

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,34 @@
 <template>
-<div style="height:100px">
-  <v-footer absolute>
-    <v-card
-      flat
-      tile
-      width="100%"
-      class="grey lighten-4 grey--text text--darken-1 text-center"
-    >
-      <v-card-text class="grey--text text--darken-1">
-       ©{{ new Date().getFullYear() }} &nbsp; HakimYu<br />
-        <a
-          class="grey--text text--darken-1"
-          style="text-decoration: none"
-          href="https://beian.miit.gov.cn/"
-          rel="noopener"
-          target="_blank"
-          >无ICP备114514号</a
-        ></v-card-text
+  <div style="height: 100px">
+    <v-footer absolute>
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="grey lighten-4 grey--text text--darken-1 text-center"
       >
-    </v-card>
-  </v-footer>
-</div>
+        <v-card-text class="grey--text text--darken-1">
+          Copyright ©{{ new Date().getFullYear() }}
+          <a
+            class="grey--text text--darken-1"
+            style="text-decoration: none"
+            href="https://hakimyu.cn/"
+            target="_blank"
+            >HakimYu</a
+          >
+          All Rights Reserved<br />
+          <a
+            class="grey--text text--darken-1"
+            style="text-decoration: none"
+            href="https://beian.miit.gov.cn/"
+            rel="noopener"
+            target="_blank"
+            >赣ICP备2022001931号</a
+          ></v-card-text
+        >
+      </v-card>
+    </v-footer>
+  </div>
 </template>
 
 <script>
