689327d4e357741bcd207fea92e8c55a857f6b5d
HakimYu <yhjredin@gmail.com>
2022-05-10 10:39:29 +0800

    add some comments, update package

--- src/components/TXNet.vue
+++ src/components/TXNet.vue
@@ -1,3 +1,13 @@
+<!--
+ * @Author: HakimYu yhjredin@gmail.com
+ * @Date: 2022-04-12 19:37:37
+ * @LastEditors: HakimYu yhjredin@gmail.com
+ * @LastEditTime: 2022-05-10 10:27:12
+ * @FilePath: \homepagebutvue\src\components\TXNet.vue
+ * @Description: txnet component
+ * 
+ * Copyright (c) 2022 by HakimYu yhjredin@gmail.com, All Rights Reserved. 
+-->
 <template>
   <v-container>
     <div class="d-flex justify-center display-1">英语学习是你自己的事</div>
@@ -57,17 +67,26 @@ export default {
     checked: false,
   }),
   methods: {
+    /**
+     * @description: post to the server to check the password
+     * @param {*}
+     * @return {*}
+     */
     checkPassword() {
       axios
         .post(`https://tools.hakimyu.cn/api/auth?password=${this.password}`)
         .then((response) => {
+          //get correct sign
           let sign = md5(
             response.data.status_code.toString() + response.data.t.toString()
           );
 
+          //if status_code is OK and sign is correct
           if (response.data.status_code == 200 && response.data.sign == sign) {
+            //set checked to true
             this.checked = true;
           } else if (response.data.sign != sign) {
+            //if sign is not correct
             alert("别扒拉了草！");
           }
         })
@@ -75,19 +94,34 @@ export default {
           console.log(error);
         });
     },
+
+    /**
+     * @description: get the questions from the file: page1
+     * @param {*}
+     * @return {*}
+     */
     get() {
       axios
         .get(`/page1?t=${Date.now()}`)
         .then((response) => {
+          //first, get the question1(the first 5 questions)
           let q1 = JSONPath.query(response.data, "$..questionList.*");
-          q1 = q1.slice(0, 5)
+          q1 = q1.slice(0, 5);
+          //then, get the question2(the next 15 questions)
           let q2 = JSONPath.query(response.data, "$..questions_list.*");
+          //add the two questions together
           this.q = q1.concat(q2);
         })
         .catch(function (error) {
           console.log(error);
         });
     },
+
+    /**
+     * @description: get the answer text(through the answer_text)
+     * @param {*} part
+     * @return {*}
+     */
     getAnswerText(part) {
       switch (part.answer_text) {
         case "A":
@@ -98,6 +132,12 @@ export default {
           return part.options[2];
       }
     },
+
+    /**
+     * @description: 
+     * @param {*} part
+     * @return {*}
+     */
     getQuestionText(part) {
       if (part.question_text.indexOf("<br") != -1) {
         return part.question_text.substring(0, part.question_text.length - 7);
